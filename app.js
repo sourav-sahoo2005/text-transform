e431e9e9627be672aba55547863fa5cdb0e9caa3
Sourav Sahoo <souravsahoo26320@gmail.com>
2025-06-08 06:49:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const usertext = document.getElementById('User_Text')
 
 const upbtn = document.getElementById('upbtn')
 const lowbtn = document.getElementById('lowbtn')
-// usertext.innerText= "hey iam sourav"
 
 const uppercase = ()=>{
     usertext.value =  usertext.value.toUpperCase();
